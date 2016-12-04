a00e14b8517363bb74238c1653015838d5547e12
Jykinturah <jykinturah@gmail.com>
2016-12-04 02:04:44 -0800

    Getting bearings

--- sona.js
+++ sona.js
@@ -3,17 +3,39 @@ const fs = require('fs');
 const Eris = require('eris'); // https://abal.moe/Eris/docs/getting-started
 const tumblrjs = require('tumblr.js'); // https://tumblr.github.io/tumblr.js/index.html
 const Auth = require('./auth.json');
-const Config = require("./config.json");
+const Config = require('./config.json');
+global.Promise = require('bluebird');
 
 // Auth stuff
-var bot = new Eris(Auth.bot_token)
-var tumblr = tumblrjs.createClient(Auth.tum_token);
+var bot = new Eris(Auth.bot_token, {
+    getAllUsers: true,
+    messageLimit: 0,
+    maxShards: 2, 
+    disableEvents: {
+        TYPING_START: true,
+        MESSAGE_UPDATE: true,
+        MESSAGE_DELETE: true,
+        MESSAGE_DELETE_BULK: true,
+        VOICE_STATE_UPDATE: true
+    }
+});
+var tumblr = tumblrjs.createClient(Auth.tum_token,);
 
 // tumblr.blogPosts('').then(resp => {console.log(resp.posts[0]);}).catch(err => {console.log(err);});
 
 // When bot starts set status "Playing with Luna"
 // Don't judge me
-bot.on("ready", () => {console.log("Fickle Dissonance is awake!");bot.editStatus("online",{"name":"with Luna"});});
+bot.on("ready", () => {
+	console.log(bot.user.username + " is awake! [" + bot.shards.size + "]");
+	console.log("Servers:" + bot.guilds.size);
+	bot.editStatus("online",{"name":"with Luna"});
+});
+
+// https://github.com/ddlr/WishBot/blob/chryssi/index.js <- reference this
+bot.on("messageCreate", (msg) => {
+	if( !bot.ready || msg.author.bot ) return;
+	if(msg.content.includes(bot.user.mention)) bot.createMessage(msg.channel.id, "Oh, hello!");
+});
 
 /*
 
